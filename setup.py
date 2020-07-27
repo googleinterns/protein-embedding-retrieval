8f5b0ebe8e026b52f8e59b2f9d0ec6d416ca15e5
amirshane <amirshanehsazzadeh@college.harvard.edu>
2020-07-27 14:52:45 -0400

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 
+
+with open('requirements.txt', 'r') as f:
+	install_requires = f.read()
+
 setup(
    name='contextual_lenses',
    version='1.0',
@@ -7,5 +11,5 @@ setup(
    author='Amir Shanehsazzadeh',
    author_email='amirshanehsaz@google.com',
    packages=['contextual_lenses'],
-   install_requires=['jax', 'jaxlib', 'flax'],
+   install_requires=install_requires,
 )
