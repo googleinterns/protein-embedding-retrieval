d29a3030882fc93a14f4a7682e33f368eb193cdf
amirshane <amirshanehsazzadeh@college.harvard.edu>
2020-08-17 23:55:20 -0400

    recomputing params

--- generate_params.py
+++ generate_params.py
@@ -229,6 +229,86 @@ for params in itertools.product(lrs, lrs, lrs, wds, wds, wds):
 					'lens_train_samples': lens_train_samples,
 					'index': index
 				 }
+	# params_combinations.append(param_dict)
+	# index_to_params[index] = param_dict
+	count += 1
+
+
+reduce_fn_name = 'linear_max_pool'
+reduce_fn_kwargs_path = 'linear_pool_1024'
+epochs = 10
+measurements = 2
+batch_size = 64
+lr_1 = 0.0
+lens_lrs = [1e-4, 5e-5, 1e-5]
+predictor_lrs = [1e-3, 5e-4, 1e-4]
+wd_1 = 0.0
+lens_wds = [0.05, 0.1, 0.2]
+predictor_wds = [0.0, 0.05, 0.1]
+train_families = 10000
+lens_train_samples = 50
+
+# Transformer experiments
+encoder_fn_name = 'transformer'
+encoder_fn_kwargs_path = 'medium_transformer_kwargs'
+use_transformer = True 
+use_bert = True 
+for params in itertools.product(lens_lrs, predictor_lrs, lens_wds, predictor_wds):
+	lr_2, lr_3, wd_2, wd_3 = params
+	index = '%08d' % count
+	param_dict = {
+					'encoder_fn_name': encoder_fn_name,
+					'encoder_fn_kwargs_path': encoder_fn_kwargs_path,
+					'reduce_fn_name': reduce_fn_name,
+					'reduce_fn_kwargs_path': reduce_fn_kwargs_path,
+					'epochs': epochs,
+					'measurements': measurements,
+					'lens_batch_size': batch_size,
+					'knn_batch_size': batch_size,
+					'lens_lr': lr_2, 
+					'predictor_lr': lr_3,
+					'lens_wd':  wd_2,
+					'predictor_wd': wd_3,
+					'train_families': train_families,
+					'lens_train_samples': lens_train_samples,
+					'use_transformer': use_transformer,
+					'use_bert': use_bert,
+					'index': index
+				 }
+	params_combinations.append(param_dict)
+	index_to_params[index] = param_dict
+	count += 1
+
+
+# Pretrained transformer experiments
+predictor_wds = [0.15, 0.2, 0.25]
+encoder_fn_name = 'transformer'
+encoder_fn_kwargs_path = 'medium_transformer_kwargs'
+restore_transformer_dir = 'gs://sequin-public/transformer_models/medium_trembl_bert/'
+use_transformer = True 
+use_bert = True 
+for params in itertools.product(lens_lrs, predictor_lrs, lens_wds, predictor_wds):
+	lr_2, lr_3, wd_2, wd_3 = params
+	index = '%08d' % count
+	param_dict = {
+					'encoder_fn_name': encoder_fn_name,
+					'encoder_fn_kwargs_path': encoder_fn_kwargs_path,
+					'reduce_fn_name': reduce_fn_name,
+					'reduce_fn_kwargs_path': reduce_fn_kwargs_path,
+					'epochs': epochs,
+					'measurements': measurements,
+					'batch_size': batch_size,
+					'lens_lr': lr_2, 
+					'predictor_lr': lr_3,
+					'lens_wd':  wd_2,
+					'predictor_wd': wd_3,
+					'train_families': train_families,
+					'lens_train_samples': lens_train_samples,
+					'use_transformer': use_transformer,
+					'use_bert': use_bert,
+					'restore_transformer_dir': restore_transformer_dir,
+					'index': index
+				 }
 	params_combinations.append(param_dict)
 	index_to_params[index] = param_dict
 	count += 1
